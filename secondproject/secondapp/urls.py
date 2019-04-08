743c56dda6f0b9f9d1f09d0bcf05b04a90860038
Your Name <you@example.com>
2019-04-08 00:01:05 -0700

    Task2 done

---
+++ secondproject/secondapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from secondapp import views
+
+
+urlpatterns=[
+    path('', views.index, name="index"),
+    path('Firstapp', views.index1, name="index1"),
+    path('Secondapp', views.index2, name="index2"),
+    path('Thirdapp', views.index3, name="index3")
+
+]
---
+++ secondproject/secondapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello Guys.")
+def index1(request):
+    return HttpResponse("Hello Guys!This is my first application.")
+def index2(request):
+    return HttpResponse("Hello Guys!This is my second application.")
+def index3(request):
+    return HttpResponse("Hello Guys!This is my third application.")
